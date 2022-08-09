2b5f4a80e4b7674482b4e3a5b06b731ef56bb87f
thatguynoe <noereyesrivas@gmail.com>
2022-08-09 18:23:28 -0400

    use whiptail instead of dialog

--- narbs.sh
+++ narbs.sh
@@ -28,33 +28,40 @@ error() {
 }
 
 welcomemsg() {
-    dialog --title "Welcome!" --msgbox "Welcome to Noe's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Noe" 10 60
+    whiptail --title "Welcome!" \
+        --msgbox "Welcome to Noe's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Noe" 10 60
 
-    dialog --colors --title "Important Note!" --yes-label "All ready!" --no-label "Return..." --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
+    whiptail --title "Important Note!" --yes-button "All ready!" \
+        --no-button "Return..." \
+        --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
 }
 
 getuserandpass() {
-    # Prompts user for new username and password.
-    name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
+    # Prompts user for new username an password.
+    name=$(whiptail --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
     while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
-        name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+        name=$(whiptail --nocancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
     done
-    pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-    pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+    pass1=$(whiptail --nocancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
+    pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
     while ! [ "$pass1" = "$pass2" ]; do
         unset pass2
-        pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-        pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+        pass1=$(whiptail --nocancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+        pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
     done
 }
 
 usercheck() {
     ! { id -u "$name" >/dev/null 2>&1; } ||
-        dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. NARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nNARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that NARBS will change $name's password to the one you just gave." 14 70
+        whiptail --title "WARNING" --yes-button "CONTINUE" \
+            --no-button "No wait..." \
+            --yesno "The user \`$name\` already exists on this system. NARBS can install for a user already existing, but it will OVERWRITE any conflicting settings/dotfiles on the user account.\\n\\nNARBS will NOT overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that NARBS will change $name's password to the one you just gave." 14 70
 }
 
 preinstallmsg() {
-    dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || {
+    whiptail --title "Let's get this party started!" --yes-button "Let's go!" \
+        --no-button "No, nevermind!" \
+        --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || {
         clear
         exit 1
     }
@@ -62,7 +69,7 @@ preinstallmsg() {
 
 adduserandpass() {
     # Adds user `$name` with password $pass1.
-    dialog --infobox "Adding user \"$name\"..." 4 50
+    whiptail --infobox "Adding user \"$name\"..." 7 50
     useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
         usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
     export repodir="/home/$name/.local/src"
@@ -75,12 +82,12 @@ adduserandpass() {
 refreshkeys() {
     case "$(readlink -f /sbin/init)" in
     *systemd*)
-        dialog --infobox "Refreshing Arch Keyring..." 4 40
+        whiptail --infobox "Refreshing Arch Keyring..." 7 40
         pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
         ;;
     *)
-        dialog --infobox "Enabling Arch Repositories..." 4 40
         pacman --noconfirm --needed -S artix-keyring artix-archlinux-support >/dev/null 2>&1
+        whiptail --infobox "Enabling Arch Repositories..." 7 40
         for repo in extra community; do
             grep -q "^\[$repo\]" /etc/pacman.conf ||
                 echo "[$repo]
@@ -99,7 +106,7 @@ newperms() { # Set special sudoers settings for install (or after).
 
 manualinstall() { # Installs $1 manually. Used only for AUR helper here.
     # Should be run after repodir is created and var is set.
-    dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
+    whiptail --infobox "Installing \"$1\", an AUR helper..." 7 50
     sudo -u "$name" mkdir -p "$repodir/$1"
     sudo -u "$name" git clone --depth 1 "https://aur.archlinux.org/$1.git" "$repodir/$1" >/dev/null 2>&1 ||
         {
@@ -110,19 +117,23 @@ manualinstall() { # Installs $1 manually. Used only for AUR helper here.
     sudo -u "$name" -D "$repodir/$1" makepkg --noconfirm -si >/dev/null 2>&1 || return 1
 }
 
-maininstall() { # Installs all needed programs from main repo.
-    dialog --title "NARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+maininstall() {
+    # Installs all needed programs from main repo.
+    whiptail --title "NARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
     installpkg "$1"
 }
 
 gitmakeinstall() {
     progname="$(basename "$1" .git)"
     dir="$repodir/$progname"
-    dialog --title "NARBS Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-    sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || {
-        cd "$dir" || return 1
-        sudo -u "$name" git pull --force origin main
-    }
+    whiptail --title "NARBS Installation" \
+        --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 8 70
+    sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
+        --no-tags -q "$1" "$dir" ||
+        {
+            cd "$dir" || return 1
+            sudo -u "$name" git pull --force origin main
+        }
     cd "$dir" || exit 1
     make >/dev/null 2>&1
     make install >/dev/null 2>&1
@@ -130,13 +141,15 @@ gitmakeinstall() {
 }
 
 aurinstall() {
-    dialog --title "NARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+    whiptail --title "NARBS Installation" \
+        --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 9 70
     echo "$aurinstalled" | grep -q "^$1$" && return 1
     sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 }
 
 pipinstall() {
-    dialog --title "NARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
+    whiptail --title "NARBS Installation" \
+        --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 9 70
     [ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
     yes | pip install "$1"
 }
@@ -157,8 +170,9 @@ installationloop() {
     done </tmp/progs.csv
 }
 
-putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-    dialog --infobox "Downloading and installing config files..." 4 60
+putgitrepo() {
+    # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
+    whiptail --infobox "Downloading and installing config files..." 7 60
     [ -z "$3" ] && branch="main" || branch="$repobranch"
     dir=$(mktemp -d)
     [ ! -d "$2" ] && mkdir -p "$2"
@@ -174,16 +188,17 @@ systembeepoff() {
 }
 
 finalize() {
-    dialog --infobox "Preparing welcome message..." 4 50
-    dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Noe" 12 80
+    whiptail --title "All done!" \
+        --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Noe" 13 80
 }
 
 ### THE ACTUAL SCRIPT ###
 
 ### This is how everything happens in an intuitive format and order.
 
-# Check if user is root on Arch distro. Install dialog.
-pacman --noconfirm --needed -Sy dialog || error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
+# Check if user is root on Arch distro. Install whiptail.
+pacman --noconfirm --needed -Sy libnewt ||
+    error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
 
 # Welcome user and pick dotfiles.
 welcomemsg || error "User exited."
@@ -203,11 +218,13 @@ preinstallmsg || error "User exited."
 refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
 for x in curl base-devel git ntp zsh; do
-    dialog --title "NARBS Installation" --infobox "Installing \`$x\` which is required to install and configure other programs." 5 70
+    whiptail --title "NARBS Installation" \
+        --infobox "Installing \`$x\` which is required to install and configure other programs." 8 70
     installpkg "$x"
 done
 
-dialog --title "NARBS Installation" --infobox "Synchronizing system time to ensure successful and secure installation of software..." 4 70
+whiptail --title "NARBS Installation" \
+    --infobox "Synchronizing system time to ensure successful and secure installation of software..." 8 70
 ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 
 adduserandpass || error "Error adding username and/or password."
@@ -236,8 +253,10 @@ manualinstall $aurhelper || error "Failed to install AUR helper."
 # and all build dependencies are installed.
 installationloop
 
-dialog --title "NARBS Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
-yes | sudo -u "$name" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
+whiptail --title "NARBS Installation" \
+    --infobox "Finally, installing the newest version of \`libxft\` to enable color emoji in suckless software without crashes." 8 70
+pacman -Qs libxft-git ||
+    yes | sudo -u "$name" $aurhelper -S libxft-git >/dev/null 2>&1
 
 # Install the dotfiles in the user's home directory
 putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"

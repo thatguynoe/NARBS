436598d77ca81ab97e710d7b76aca896788e4eb4
thatguynoe <noereyesrivas@gmail.com>
2022-08-09 16:29:35 -0400

    format narbs.sh through `shfmt`

--- narbs.sh
+++ narbs.sh
@@ -11,7 +11,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
     p) progsfile=${OPTARG} ;;
     a) aurhelper=${OPTARG} ;;
     *) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
-esac done
+    esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/thatguynoe/dots.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/thatguynoe/NARBS/main/progs.csv"
@@ -20,17 +20,20 @@ esac done
 
 ### FUNCTIONS ###
 
-installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
+installpkg() { pacman --noconfirm --needed -S "$1" >/dev/null 2>&1; }
 
-error() { echo "ERROR: $1" ; exit 1;}
+error() {
+    echo "ERROR: $1"
+    exit 1
+}
 
-welcomemsg() { \
+welcomemsg() {
     dialog --title "Welcome!" --msgbox "Welcome to Noe's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Noe" 10 60
 
     dialog --colors --title "Important Note!" --yes-label "All ready!" --no-label "Return..." --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
-    }
+}
 
-getuserandpass() { \
+getuserandpass() {
     # Prompts user for new username and password.
     name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
     while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
@@ -42,55 +45,67 @@ getuserandpass() { \
         unset pass2
         pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
         pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-    done ;}
+    done
+}
 
-usercheck() { \
+usercheck() {
     ! { id -u "$name" >/dev/null 2>&1; } ||
-    dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. NARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nNARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that NARBS will change $name's password to the one you just gave." 14 70
-    }
+        dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. NARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nNARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that NARBS will change $name's password to the one you just gave." 14 70
+}
 
-preinstallmsg() { \
-    dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit 1; }
+preinstallmsg() {
+    dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || {
+        clear
+        exit 1
     }
+}
 
-adduserandpass() { \
+adduserandpass() {
     # Adds user `$name` with password $pass1.
     dialog --infobox "Adding user \"$name\"..." 4 50
     useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-    usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
-    export repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
+        usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+    export repodir="/home/$name/.local/src"
+    mkdir -p "$repodir"
+    chown -R "$name":wheel "$(dirname "$repodir")"
     echo "$name:$pass1" | chpasswd
-    unset pass1 pass2 ;}
+    unset pass1 pass2
+}
 
-refreshkeys() { \
+refreshkeys() {
     case "$(readlink -f /sbin/init)" in
-        *systemd* )
-            dialog --infobox "Refreshing Arch Keyring..." 4 40
-            pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-            ;;
-        *)
-            dialog --infobox "Enabling Arch Repositories..." 4 40
-            pacman --noconfirm --needed -S artix-keyring artix-archlinux-support >/dev/null 2>&1
-            for repo in extra community; do
-                grep -q "^\[$repo\]" /etc/pacman.conf ||
-                    echo "[$repo]
-Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-            done
-            pacman -Sy >/dev/null 2>&1
-            pacman-key --populate archlinux
-            ;;
-    esac ;}
+    *systemd*)
+        dialog --infobox "Refreshing Arch Keyring..." 4 40
+        pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
+        ;;
+    *)
+        dialog --infobox "Enabling Arch Repositories..." 4 40
+        pacman --noconfirm --needed -S artix-keyring artix-archlinux-support >/dev/null 2>&1
+        for repo in extra community; do
+            grep -q "^\[$repo\]" /etc/pacman.conf ||
+                echo "[$repo]
+Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
+        done
+        pacman -Sy >/dev/null 2>&1
+        pacman-key --populate archlinux
+        ;;
+    esac
+}
 
 newperms() { # Set special sudoers settings for install (or after).
     sed -i "/#NARBS/d" /etc/sudoers
-    echo "$* #NARBS" >> /etc/sudoers ;}
+    echo "$* #NARBS" >>/etc/sudoers
+}
 
 manualinstall() { # Installs $1 manually. Used only for AUR helper here.
     # Should be run after repodir is created and var is set.
     dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
     sudo -u "$name" mkdir -p "$repodir/$1"
     sudo -u "$name" git clone --depth 1 "https://aur.archlinux.org/$1.git" "$repodir/$1" >/dev/null 2>&1 ||
-        { cd "$repodir/$1" || return 1 ; sudo -u "$name" git pull --force origin master;}
+        {
+            cd "$repodir/$1" || return 1
+            sudo -u "$name" git pull --force origin master
+        }
     cd "$repodir/$1"
     sudo -u "$name" -D "$repodir/$1" makepkg --noconfirm -si >/dev/null 2>&1 || return 1
 }
@@ -98,44 +113,49 @@ manualinstall() { # Installs $1 manually. Used only for AUR helper here.
 maininstall() { # Installs all needed programs from main repo.
     dialog --title "NARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
     installpkg "$1"
-    }
+}
 
 gitmakeinstall() {
     progname="$(basename "$1" .git)"
     dir="$repodir/$progname"
     dialog --title "NARBS Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-    sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin main;}
+    sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || {
+        cd "$dir" || return 1
+        sudo -u "$name" git pull --force origin main
+    }
     cd "$dir" || exit 1
     make >/dev/null 2>&1
     make install >/dev/null 2>&1
-    cd /tmp || return 1 ;}
+    cd /tmp || return 1
+}
 
-aurinstall() { \
+aurinstall() {
     dialog --title "NARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
     echo "$aurinstalled" | grep -q "^$1$" && return 1
     sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
-    }
+}
 
-pipinstall() { \
+pipinstall() {
     dialog --title "NARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
     [ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
     yes | pip install "$1"
-    }
+}
 
-installationloop() { \
-    ([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
-    total=$(wc -l < /tmp/progs.csv)
+installationloop() {
+    ([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' >/tmp/progs.csv
+    total=$(wc -l </tmp/progs.csv)
     aurinstalled=$(pacman -Qqm)
     while IFS=, read -r tag program comment; do
-        n=$((n+1))
+        n=$((n + 1))
         echo "$comment" | grep -q "^\".*\"$" && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
         case "$tag" in
-            "A") aurinstall "$program" "$comment" ;;
-            "G") gitmakeinstall "$program" "$comment" ;;
-            "P") pipinstall "$program" "$comment" ;;
-            *) maininstall "$program" "$comment" ;;
+        "A") aurinstall "$program" "$comment" ;;
+        "G") gitmakeinstall "$program" "$comment" ;;
+        "P") pipinstall "$program" "$comment" ;;
+        *) maininstall "$program" "$comment" ;;
         esac
-    done < /tmp/progs.csv ;}
+    done </tmp/progs.csv
+}
 
 putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
     dialog --infobox "Downloading and installing config files..." 4 60
@@ -145,16 +165,18 @@ putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwrit
     chown -R "$name":wheel "$dir" "$2"
     sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
     sudo -u "$name" cp -rfT "$dir" "$2"
-    }
+}
 
-systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+systembeepoff() {
+    dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
     rmmod pcspkr
-    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
+    echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf
+}
 
-finalize(){ \
+finalize() {
     dialog --infobox "Preparing welcome message..." 4 50
     dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Noe" 12 80
-    }
+}
 
 ### THE ACTUAL SCRIPT ###
 
@@ -256,10 +278,10 @@ Target = bash
 Description = Re-pointing /bin/sh symlink to dash...
 When = PostTransaction
 Exec = /usr/bin/ln -sfT dash /usr/bin/sh
-Depends = dash' > /usr/share/libalpm/hooks/dash.hook
+Depends = dash' >/usr/share/libalpm/hooks/dash.hook
 
 # dbus UUID must be generated for Artix runit.
-dbus-uuidgen > /var/lib/dbus/machine-id
+dbus-uuidgen >/var/lib/dbus/machine-id
 
 # Automatic timezone detection through a WIFI connection.
 [ ! -f /etc/NetworkManager/dispatcher.d/09-timezone ] && printf '#!/bin/sh
@@ -284,11 +306,11 @@ esac' >/etc/NetworkManager/dispatcher.d/09-timezone && chmod +x /etc/NetworkMana
         Option "FingerLow" "30"
         Option "FingerHigh" "50"
         Option "MaxTapTime" "125"
-EndSection' > /etc/X11/xorg.conf.d/30-synaptics.conf
+EndSection' >/etc/X11/xorg.conf.d/30-synaptics.conf
 
 # Fix fluidsynth/pulseaudio issue.
 grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
-    echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
+    echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >>/etc/conf.d/fluidsynth
 
 # This line, overwriting the `newperms` command above will allow the user to
 # run serveral important commands, `shutdown`, `reboot`, updating, etc. without

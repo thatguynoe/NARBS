e6a325e4a28b9048962eee3d85b6fee87a12b5f2
thatguynoe <noereyesrivas@gmail.com>
2023-11-30 19:54:21 -0500

    better implementation of refreshkeys

--- narbs.sh
+++ narbs.sh
@@ -75,34 +75,22 @@ adduserandpass() {
 }
 
 refreshkeys() {
-    case "$(readlink -f /sbin/init)" in
-    *systemd*)
-        whiptail --infobox "Refreshing Arch Keyring..." 7 40
-        pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-        ;;
-    *)
-        whiptail --infobox "Enabling Arch Repositories..." 7 40
-        if ! grep -q "^\[universe\]" /etc/pacman.conf; then
-            echo "[universe]
-Server = https://universe.artixlinux.org/\$arch
-Server = https://mirror1.artixlinux.org/universe/\$arch
-Server = https://mirror.pascalpuffke.de/artix-universe/\$arch
-Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/\$arch
-Server = https://mirror1.cl.netactuate.com/artix/universe/\$arch
-Server = https://ftp.crifo.org/artix-universe/" >>/etc/pacman.conf
-            pacman -Sy --noconfirm >/dev/null 2>&1
-        fi
-        pacman --noconfirm --needed -S \
-            artix-keyring artix-archlinux-support >/dev/null 2>&1
-        for repo in extra community; do
-            grep -q "^\[$repo\]" /etc/pacman.conf ||
-                echo "[$repo]
+	case "$(readlink -f /sbin/init)" in
+	*systemd*)
+		whiptail --infobox "Refreshing Arch Keyring..." 7 40
+		pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
+		;;
+	*)
+		whiptail --infobox "Enabling Arch Repositories for more a more extensive software collection..." 7 40
+		pacman --noconfirm --needed -S \
+			artix-keyring artix-archlinux-support >/dev/null 2>&1
+		grep -q "^\[extra\]" /etc/pacman.conf ||
+			echo "[extra]
 Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
-        done
-        pacman -Sy >/dev/null 2>&1
-        pacman-key --populate archlinux >/dev/null 2>&1
-        ;;
-    esac
+		pacman -Sy --noconfirm >/dev/null 2>&1
+		pacman-key --populate archlinux >/dev/null 2>&1
+		;;
+	esac
 }
 
 manualinstall() {
